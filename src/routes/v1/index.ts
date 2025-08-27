5362476980187a1a9187d2567b205e1a0eb84f3e
Md Asharaf <718romanempire@gmail.com>
2025-08-27 20:53:08 +0530

    exposed user,public and admin routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./auth";
-
+import userRoutes from "./user";
+import publicRoutes from "./public";
+import adminRoutes from "./admin";
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/user", userRoutes);
+router.use("/public", publicRoutes);
+router.use("/admin", adminRoutes);
 
 export default router;
